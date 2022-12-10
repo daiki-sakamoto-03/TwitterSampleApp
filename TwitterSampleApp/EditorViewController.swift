7c41d6c4a454839bd02e02223e587f874cb5922e
daikisakamoto <daiki19910717@gmail.com>
2022-12-10 22:22:46 +0900

    ツイート作成ボタン追加・投稿画面作成

---
+++ TwitterSampleApp/EditorViewController.swift
@@ -0,0 +1,10 @@
+//
+//  EditorViewController.swift
+//  TwitterSampleApp
+//
+//  Created by 坂本　大貴 on 2022/12/10.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {}
--- TwitterSampleApp/ViewController.swift
+++ TwitterSampleApp/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
     @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func addButton(_ sender: UIButton) {
+        transitionToEditorView()
+    }
     
 
     override func viewDidLoad() {
@@ -19,17 +21,30 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         navigationItem.title = "ホーム"
+        configureButton()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
         cell.userName.text = "Daiki"
-        cell.tweetText.text = "このテキストが改行するかのテストをしたいので、長文を入力してみます。"
+        cell.tweetText.text = "このテキストが改行するかのテストをしたいので、長文を入力してみます。このテキストが改行するかのテストをしたいので、長文を入力してみます。このテキストが改行するかのテストをしたいので、長文を入力してみます。このテキストが改行するかのテストをしたいので、長文を入力してみます。"
         return cell
+    }
+    
+    // ボタンを丸くするメソッド
+    func configureButton() {
+        addButton.layer.cornerRadius = addButton.bounds.width / 2
+    }
+    
+    // ＋ボタンがタップされた際のメソッド
+    func transitionToEditorView() {
+        let storyboard = UIStoryboard(name: "EditorViewController", bundle: nil)
+        guard let editorViewController = storyboard.instantiateInitialViewController() as? EditorViewController else { return }
+        present(editorViewController, animated: true)
     }
 
 
